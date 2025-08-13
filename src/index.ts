06e1caa74175e226fe2b8e874ced4a72fd4ba26d
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-13 16:22:22 -0500

    ✨ Configure http server and index file

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { createServer } from "http";
+import path from "path";
+import { SocketIoServer } from "./SocketIoServer";
+import router from "./routes/index";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new SocketIoServer(httpServer);
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(router);
+
+httpServer.listen(app.get("port"), () => {
+  console.log(`Server on http://localhost:${app.get("port")}`);
+});
+
+io.start();
